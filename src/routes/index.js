d7f3b1127a568188ea1e9926b124e186e88ac88e
Masila <abelmasila@gmail.com>
2019-07-03 16:07:43 +0300

    added all crud for book model

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import Users from "./../controllers/user";
+import Books from "./../controllers/book";
 export default app => {
   app.get("/api", (req, res) =>
     res.status(200).send({
@@ -7,4 +8,8 @@ export default app => {
   );
 
   app.post("/api/users", Users.signUp); // API route for user to signup
+  app.post("/api/users/:userId/books", Books.create); // API route for user to create a book
+  app.get("/api/books", Books.list); // API route for user to get all books in the database
+  app.put("/api/books/:bookId", Books.modify); // API route for user to edit a book
+  app.delete("/api/books/:bookId", Books.delete); // API route for user to delete a book
 };
