62ad1ca9a576a4037e13a530badb1bc12c0c449d
Masila <abelmasila@gmail.com>
2019-07-03 15:13:05 +0300

    added port env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+//dotenv init
+dotenv.config();
 
 //initialize express app
 const app = express();
+
+//app use cors middleware
+app.use(cors());
 
 // GET /
 app.get("/", (req, res, next) => {
   res.send("Hello browser");
 });
-app.listen(3000, () => {
-  console.log("App running on port 30000");
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`);
 });
